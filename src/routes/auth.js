35aeff549937c99a1f29ec27cd0ac5ae32358516
sumit kumar <sumit.kumar@marktine.com>
2025-03-24 22:37:13 +0530

    worked on profile edit and logout api.

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -44,4 +44,11 @@ authRouter.post("/login", async (req, res) => {
     }
 });
 
+authRouter.post("/logout", (req, res)=>{
+    res.cookie("token", null, {
+        expires: new Date(Date.now())
+    })
+    res.send("logout successfull");
+})
+
 module.exports=authRouter;
--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,5 +1,7 @@
 const express= require("express");
 const { userAuth } = require("../utils/auth");
+const { validationEditProfileData } = require("../utils/validation");
+const { User } = require("../models/user");
 
 const profileRouter=express.Router();
 
@@ -16,5 +18,20 @@ profileRouter.get("/profile/view", userAuth, async (req, res) => {
         throw new Error(err.message);
     }
 })
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+    try {
+        if(!validationEditProfileData(req)){
+            throw new Error("Invalid Edit Request");
+        }
+        const loggedinUser=req.user;
+        Object.keys(req.body).forEach((key)=> (loggedinUser[key] = req.body[key]))
+        await new User(loggedinUser).save();
+        res.send({message:"Edited successfully..", data:loggedinUser})
+
+    } catch (err) {
+        res.status(400).send("ERROR: " + err.message);
+    }
+});
 
 module.exports=profileRouter;
